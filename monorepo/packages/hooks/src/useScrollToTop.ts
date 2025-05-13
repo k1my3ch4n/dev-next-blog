b453e19c0594297ce277a8e86403418981e7962d
Kim <kevin2695@naver.com>
2025-05-13 22:00:10 +0900

    feat : hook packages 설정 추가 및 빌드 설정

---
+++ monorepo/packages/hooks/src/useScrollToTop.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+const useScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    setIsVisible(window.scrollY > 0);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return {
+    isVisible,
+    scrollToTop,
+  };
+};
+
+export default useScrollToTop
