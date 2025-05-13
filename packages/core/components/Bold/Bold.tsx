3a6ffadf20e644170a57ca6cf95e0847a92d1d80
Kim <kevin2695@naver.com>
2025-05-13 17:56:00 +0900

    delete : 패키지 삭제

--- packages/core/components/Bold/Bold.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './Bold.module.scss';
-
-const Bold = ({ children }: { children: React.ReactNode }) => {
-  return <span className={styles.bold}>{children}</span>;
-};
-
-export default Bold;
