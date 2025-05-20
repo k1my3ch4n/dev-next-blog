311051d0fc3e5b5e3e1f8dee8def24ef9e815cea
Kim <kevin2695@naver.com>
2025-05-20 23:11:24 +0900

    feat : components package export 수정

--- monorepo/packages/components/src/index.ts
+++ monorepo/packages/components/src/index.ts
@@ -1,3 +1,29 @@
+import Bold from "./Bold";
+import Divider from "./Divider";
+import Highlight from "./Highlight";
+import HomeButton from "./HomeButton";
+import ImageBox from "./ImageBox";
+import Layout from "./Layout";
+import Link from "./Link";
+import List from "./List";
+import NumberList from "./NumberList";
+import PageBox from "./PageBox";
+import SplitGrid from "./SplitGrid";
 import Text from "./Text";
+import Title from "./Title";
 
-export { Text };
+export {
+  Bold,
+  Divider,
+  Highlight,
+  HomeButton,
+  ImageBox,
+  Layout,
+  Link,
+  List,
+  NumberList,
+  PageBox,
+  SplitGrid,
+  Text,
+  Title,
+};
