8facc2fadd58049c0fc85dd6875d3a8d62617f4e
Kim <kevin2695@naver.com>
2025-05-22 09:13:50 +0900

    feat : components Title css 설정

--- monorepo/packages/components/src/Title.tsx
+++ monorepo/packages/components/src/Title.tsx
@@ -1,7 +1,5 @@
 const Title = ({ title }: { title: string }) => {
-  // todo : font-size 추가 ( font-size: 40px; )
-  // todo : line-height 추가 ( line-height: 1.2; )
-  const className = "font-bold py-3 px-2";
+  const className = "font-bold py-[3px] px-[2px] text-title";
 
   return <h1 className={className}>{title}</h1>;
 };
