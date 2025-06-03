08acc5ec0c94ca6464deb2630910e47ac92dad5e
Kim <kevin2695@naver.com>
2025-06-03 11:35:13 +0900

    feat : layout 설정

--- monorepo/apps/blog/src/app/layout.tsx
+++ monorepo/apps/blog/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./reset.css";
 import "./globals.css";
+import "@repo/components/index.css";
 
 import WrapperLayout from "@/components/WrapperLayout";
 import { PaperLogyFont } from "./fonts/fonts";
