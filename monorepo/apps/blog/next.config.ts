616a15eede0d451d65f99e3b52d9c5c07be88025
Kim <kevin2695@naver.com>
2025-06-03 11:10:53 +0900

    feat : 의존성 설치 및 next config 설정 추가

--- monorepo/apps/blog/next.config.ts
+++ monorepo/apps/blog/next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: [
+    "@repo/components",
+    "@repo/tailwind-config",
+    "@repo/eslint-config",
+    "@repo/typescript-config",
+  ],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
