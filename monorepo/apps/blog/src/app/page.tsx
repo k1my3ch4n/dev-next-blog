bab16d14cb44d24ee5a1a46020f318c431c13827
Kim <kevin2695@naver.com>
2025-06-03 12:32:54 +0900

    feat : Projects 생성 및 적용

--- monorepo/apps/blog/src/app/page.tsx
+++ monorepo/apps/blog/src/app/page.tsx
@@ -1,5 +1,11 @@
 import Introduce from "./_components/Introduce";
+import Projects from "./_components/Projects";
 
 export default function Home() {
-  return <Introduce />;
+  return (
+    <>
+      <Introduce />
+      <Projects />
+    </>
+  );
 }
