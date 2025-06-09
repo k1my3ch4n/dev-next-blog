b2c4c45a375296cd0374d7d6c6a3d045911d9774
Kim <kevin2695@naver.com>
2025-06-09 21:06:38 +0900

    feat : context 컴포넌트 분리 및 blog 목록 페이지에 적용

---
+++ monorepo/apps/blog/src/app/blog/layout.tsx
@@ -0,0 +1,19 @@
+import HomeButtonWrapper from "@/components/HomeButtonWrapper";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "게시글 목록",
+};
+
+export default function BlogLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <HomeButtonWrapper />
+      {children}
+    </>
+  );
+}
