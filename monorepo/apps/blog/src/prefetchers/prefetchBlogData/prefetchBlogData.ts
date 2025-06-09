45aae364db2cdc6584ba2df99617f89c5f44c7fb
Kim <kevin2695@naver.com>
2025-06-09 17:38:11 +0900

    feat : prefetchBlogData 생성

---
+++ monorepo/apps/blog/src/prefetchers/prefetchBlogData/prefetchBlogData.ts
@@ -0,0 +1,14 @@
+import { client } from "@/client/client";
+import { GET_POSTS, GET_TAGS } from "@/graphql/post";
+
+const prefetchBlogData = async () => {
+  await client.query({
+    query: GET_TAGS,
+  });
+
+  await client.query({
+    query: GET_POSTS,
+  });
+};
+
+export default prefetchBlogData;
