bc97b91f7c56da0c35c9e26064e63659bdddcad2
Kim <kevin2695@naver.com>
2025-06-10 21:21:10 +0900

    feat : prefetchHomeData 적용

--- monorepo/apps/blog/src/app/page.tsx
+++ monorepo/apps/blog/src/app/page.tsx
@@ -1,9 +1,12 @@
+import prefetchHomeData from "@/prefetchers/prefetchHomeData";
 import Blog from "./_components/Blog";
 import Extra from "./_components/Extra";
 import Introduce from "./_components/Introduce";
 import Projects from "./_components/Projects";
 
-export default function Home() {
+export default async function Home() {
+  await prefetchHomeData();
+
   return (
     <>
       <Introduce />
