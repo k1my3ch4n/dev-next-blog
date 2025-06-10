20991d554f8708ac0c54c817ee199acd9b5a0a9e
Kim <kevin2695@naver.com>
2025-06-10 22:31:11 +0900

    feat : @apollo/client-integration-nextjs 설정 후 ApolloNextAppProvider 적용

--- monorepo/apps/blog/src/client/client.ts
+++ monorepo/apps/blog/src/client/client.ts
@@ -1,35 +1,21 @@
-import { ApolloClient, HttpLink } from "@apollo/client";
+import { HttpLink } from "@apollo/client";
 
 import {
   registerApolloClient,
   InMemoryCache,
+  ApolloClient,
 } from "@apollo/client-integration-nextjs";
 
 const VALIDATED_API_KEY = process.env.NEXT_PUBLIC_VALIDATED_API_KEY || "";
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
+    cache: new InMemoryCache(),
     link: new HttpLink({
       uri: process.env.NEXT_PUBLIC_SERVER_URI,
       headers: {
         "x-api-key": VALIDATED_API_KEY,
       },
     }),
-    cache: new InMemoryCache(),
-    ssrMode: true,
   });
 });
-
-export const client = () => {
-  const httpLink = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_SERVER_URI,
-    headers: {
-      "x-api-key": VALIDATED_API_KEY,
-    },
-  });
-
-  return new ApolloClient({
-    cache: new InMemoryCache(),
-    link: httpLink,
-  });
-};
--- monorepo/apps/blog/src/components/ApolloWrapper/ApolloWrapper.tsx
+++ monorepo/apps/blog/src/components/ApolloWrapper/ApolloWrapper.tsx
@@ -1,10 +1,34 @@
 "use client";
 
-import { ApolloProvider } from "@apollo/client";
-import { client } from "@/client/client";
+import { HttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloNextAppProvider,
+  InMemoryCache,
+} from "@apollo/client-integration-nextjs";
+
+const VALIDATED_API_KEY = process.env.NEXT_PUBLIC_VALIDATED_API_KEY || "";
+
+export const makeClient = () => {
+  const httpLink = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_SERVER_URI,
+    headers: {
+      "x-api-key": VALIDATED_API_KEY,
+    },
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink,
+  });
+};
 
 const ApolloWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
 };
 
 export default ApolloWrapper;
