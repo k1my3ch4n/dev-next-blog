223922c7bd6b935066c25b5d753fcb0d6f66e7d9
Kim <kevin2695@naver.com>
2025-06-10 21:56:49 +0900

    feat : client 에 ssr 적용

--- monorepo/apps/blog/src/client/client.ts
+++ monorepo/apps/blog/src/client/client.ts
@@ -7,16 +7,6 @@ import {
 
 const VALIDATED_API_KEY = process.env.NEXT_PUBLIC_VALIDATED_API_KEY || "";
 
-export const client = new ApolloClient({
-  link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_SERVER_URI,
-    headers: {
-      "x-api-key": VALIDATED_API_KEY,
-    },
-  }),
-  cache: new InMemoryCache(),
-});
-
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     link: new HttpLink({
@@ -29,3 +19,17 @@ export const { getClient } = registerApolloClient(() => {
     ssrMode: true,
   });
 });
+
+export const client = () => {
+  const httpLink = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_SERVER_URI,
+    headers: {
+      "x-api-key": VALIDATED_API_KEY,
+    },
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink,
+  });
+};
