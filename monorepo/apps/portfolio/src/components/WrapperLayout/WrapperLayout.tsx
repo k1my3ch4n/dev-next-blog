df69c7a5d8978a6241a03d0bd76f8914dd12c77e
Kim <kevin2695@naver.com>
2025-06-16 10:37:23 +0900

    delete : WrapperLayout 삭제

--- monorepo/apps/portfolio/src/components/WrapperLayout/WrapperLayout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Layout } from "@repo/components";
-
-const WrapperLayout = ({ children }: { children: React.ReactNode }) => {
-  return <Layout>{children}</Layout>;
-};
-
-export default WrapperLayout;
