7f358e4ada07188efd77c1869bebc0fe5d94ad1e
Kim <kevin2695@naver.com>
2025-06-23 18:48:58 +0900

    feat : mdx 중복 디자인 제거

--- monorepo/apps/blog/src/components/MdxComponents/MdxComponents.tsx
+++ monorepo/apps/blog/src/components/MdxComponents/MdxComponents.tsx
@@ -3,9 +3,7 @@ import "highlight.js/styles/1c-light.css";
 
 const MdxComponents = (components: MDXComponents): MDXComponents => {
   return {
-    p: ({ children }) => (
-      <p className="text-[20px] font-normal leading-normal">{children}</p>
-    ),
+    p: ({ children }) => <p className="font-normal">{children}</p>,
     h2: ({ children }) => (
       <h2 className="text-[32px] font-bold my-[16px]">{children}</h2>
     ),
@@ -37,15 +35,17 @@ const MdxComponents = (components: MDXComponents): MDXComponents => {
 
         return (
           <>
-            <p className="font-normal text-[12px] absolute right-[10px] bg-transparent">
+            <p className="font-normal text-[12px] absolute top-[2px] right-[5px] bg-transparent">
               {preLanguage}
             </p>
-            <code className="p-[4px] font-normal">{children}</code>
+            <code className="text-[16px] p-[4px] font-normal font-[Paperlogy]">
+              {children}
+            </code>
           </>
         );
       } else {
         return (
-          <code className="text-[16px] p-[4px] text-[#eb5757] bg-[#87837826] rounded-[4px] font-semibold">
+          <code className="text-[16px] p-[2px] text-[#eb5757] bg-[#87837826] rounded-[4px] font-semibold font-[Paperlogy]">
             {children}
           </code>
         );
