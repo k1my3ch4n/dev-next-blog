a6a7fbe7b6bddad5a2a2b8df30555e179c1e6b1b
Kim <kevin2695@naver.com>
2025-07-09 00:49:52 +0900

    feat : robot.txt -> robot.ts 로 수정

---
+++ apps/portfolio/src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://portfolio.k1my3ch4n.xyz/sitemap.xml",
+  };
+}
