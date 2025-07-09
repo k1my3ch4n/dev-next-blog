aedeca1d7be99b01b55c32b0f66377e7066c863a
Kim <kevin2695@naver.com>
2025-07-09 18:26:39 +0900

    feat : prefetchHomeData 가 데이터를 리턴하도록 설정

--- apps/blog/src/prefetcher/prefetchHomeData/prefetchHomeData.ts
+++ apps/blog/src/prefetcher/prefetchHomeData/prefetchHomeData.ts
@@ -1,11 +1,14 @@
+import { PostsResponseData } from "@/fixtures/posts";
 import { getClient } from "@client/client";
 import { GET_POSTS } from "@graphql/post";
 
 const prefetchHomeData = async () => {
-  await getClient().query({
+  const { data } = await getClient().query<PostsResponseData>({
     query: GET_POSTS,
     variables: { tag: "", orderBy: "DESC" },
   });
+
+  return { data };
 };
 
 export default prefetchHomeData;
