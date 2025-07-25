5f9eb2e434d28951add3e8e028c55623df80d1f3
Kim <kevin2695@naver.com>
2025-07-25 21:34:43 +0900

    feat : layout 좌우 padding 수정

--- packages/components/src/Layout.tsx
+++ packages/components/src/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({
   className?: string;
   children: React.ReactNode;
 }) => {
-  const wrapperClassName = `flex flex-col m-auto my-[40px] w-2/4 min-w-[800px] max-w-[920px] leading-normal ${className}`;
+  const wrapperClassName = `flex flex-col m-auto my-[40px] w-2/4 min-w-[800px] max-w-[920px] leading-normal px-[20px] ${className}`;
 
   return <div className={wrapperClassName}>{children}</div>;
 };
