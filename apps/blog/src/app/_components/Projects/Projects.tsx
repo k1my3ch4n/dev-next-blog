e26fa56734f6e5253b7c497e221c8d841c64fc0d
Kim <kevin2695@naver.com>
2025-07-25 22:05:45 +0900

    feat : Blog 페이지에 새 개인 프로젝트 추가

--- apps/blog/src/app/_components/Projects/Projects.tsx
+++ apps/blog/src/app/_components/Projects/Projects.tsx
@@ -3,8 +3,11 @@
 import { Divider, PageBox, Title } from "@repo/components";
 import { MainLogo } from "@images";
 
-const GITHUB_REPO_LINK =
+const GITHUB_VITE_REPO_LINK =
   "https://github.com/k1my3ch4n/dev-blog/blob/main/README.md";
+
+const GITHUB_NEXT_REPO_LINK =
+  "https://github.com/k1my3ch4n/dev-next-blog#monorepo-%EB%A1%9C-%EB%B8%94%EB%A1%9C%EA%B7%B8--%ED%8F%AC%ED%8A%B8%ED%8F%B4%EB%A6%AC%EC%98%A4-%EA%B5%AC%ED%98%84-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8--next-";
 
 const Projects = () => {
   const handleClick = (link: string) => {
@@ -15,13 +18,22 @@ const Projects = () => {
     <div className="w-full mb-[20px]">
       <Title title="개인 프로젝트" />
       <Divider />
-      <PageBox
-        Thumbnail={MainLogo}
-        title="Monorepo로 블로그 및 포트폴리오 페이지 생성"
-        onClick={() => handleClick(GITHUB_REPO_LINK)}
-        width="400px"
-        height="300px"
-      />
+      <div className="w-full grid grid-cols-2 gap-[10px]">
+        <PageBox
+          Thumbnail={MainLogo}
+          title="Monorepo 마이그레이션 (Vite -> Nextjs)"
+          onClick={() => handleClick(GITHUB_NEXT_REPO_LINK)}
+          width="400px"
+          height="300px"
+        />
+        <PageBox
+          Thumbnail={MainLogo}
+          title="Monorepo로 블로그 및 포트폴리오 페이지 생성"
+          onClick={() => handleClick(GITHUB_VITE_REPO_LINK)}
+          width="400px"
+          height="300px"
+        />
+      </div>
     </div>
   );
 };
