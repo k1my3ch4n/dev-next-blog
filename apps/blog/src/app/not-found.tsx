783b8f877665833238479cc8f2aa3a963f5c95da
Kim <kevin2695@naver.com>
2025-07-28 23:46:32 +0900

    feat : 기본 NotFound  추가

---
+++ apps/blog/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center min-h-screen my-auto p-[20px] text-[#343a40]">
+      <h1 style={{ fontSize: "4rem", color: "#6c757d", marginBottom: "15px" }}>
+        404 Not Found
+      </h1>
+      <h2 className="text-5xl mb-[20px]">페이지를 찾을 수 없습니다.</h2>
+      <p className="text-xl text-center md-[30px]">
+        요청하신 페이지가 존재하지 않거나, 잘못된 주소입니다.
+      </p>
+    </div>
+  );
+}
