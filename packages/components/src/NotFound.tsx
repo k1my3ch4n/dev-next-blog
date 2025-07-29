89b13f28d1fb463a9afcaf4ccb4971d256231cc7
Kim <kevin2695@naver.com>
2025-07-29 14:46:23 +0900

    fix : Notfound 컴포넌트 디자인 수정

--- packages/components/src/NotFound.tsx
+++ packages/components/src/NotFound.tsx
@@ -2,11 +2,9 @@ import Link from "./Link";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen my-auto p-[20px] text-[#343a40]">
-      <h1 style={{ fontSize: "4rem", color: "#6c757d", marginBottom: "15px" }}>
-        404 Not Found
-      </h1>
-      <h2 className="text-5xl mb-[20px]">페이지를 찾을 수 없습니다.</h2>
+    <div className="flex flex-col items-center min-h-screen my-auto p-[30px] text-[#343a40]">
+      <h1 className="text-7xl mb-[30px] text-[#6c757d]">404 Not Found</h1>
+      <h2 className="text-5xl mb-[30px]">페이지를 찾을 수 없습니다.</h2>
       <p className="text-xl text-center mb-[30px]">
         요청하신 페이지가 존재하지 않거나, 잘못된 주소입니다.
       </p>
