9319132795a85041866a02f2ce2787971278ddda
Kim <kevin2695@naver.com>
2025-07-30 12:41:27 +0900

    fix : 블로그 게시글 목록 페이지 metadata 수정

--- apps/blog/src/app/blog/layout.tsx
+++ apps/blog/src/app/blog/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 
+const BASE_URL = "https://blog.k1my3ch4n.xyz";
+
 export const metadata: Metadata = {
   title: "게시글 목록",
   description: "게시글 목록 페이지 입니다.",
   alternates: {
-    canonical: "./blog",
+    canonical: `${BASE_URL}/blog`,
   },
 };
 
