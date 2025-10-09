0d8db3cd75bf5d3f770b685b90336ca83c23a80e
Kim <kevin2695@naver.com>
2025-10-09 18:13:36 +0900

    feat : Title 컴포넌트에 className 추가

--- packages/components/src/Title.tsx
+++ packages/components/src/Title.tsx
@@ -1,7 +1,13 @@
-const Title = ({ title }: { title: string }) => {
-  const className = "font-bold py-[3px] px-[2px] text-title";
+const Title = ({
+  className = "",
+  title,
+}: {
+  className?: string;
+  title: string;
+}) => {
+  const classNames = "font-bold py-[3px] px-[2px] text-title";
 
-  return <h1 className={className}>{title}</h1>;
+  return <h1 className={`${classNames} ${className}`}>{title}</h1>;
 };
 
 export default Title;
