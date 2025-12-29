78adb2fb21d7bbda7b0946af497f3f3228c93d15
Kim <kevin2695@naver.com>
2025-12-29 22:50:10 +0900

    refactor : PageBox 통일화 추가

--- packages/components/src/PageBox.tsx
+++ packages/components/src/PageBox.tsx
@@ -1,39 +1,29 @@
-// todo : imageBox , pageBox 크기 설정 추가 예정
-
-import ImageBox from "./ImageBox";
-
 const PageBox = ({
   Thumbnail,
   title,
   onClick,
-  width,
-  height,
-  imageWrapperClassName = "",
-  imageClassName = "",
+  className = "",
 }: {
   Thumbnail: React.FC<React.SVGProps<SVGSVGElement>>;
   title: string;
   onClick: () => void;
-  width: string;
-  height: string;
-  imageWrapperClassName?: string;
-  imageClassName?: string;
+  className?: string;
 }) => {
-  const boxClassName =
-    "w-[400px] cursor-pointer shadow-inner-border rounded-[10px] h-[fit-content]";
+  const boxClassName = `cursor-pointer shadow-inner-border rounded-[10px] overflow-hidden ${className}`;
+
+  const imageWrapperClassName =
+    "aspect-[4/3] flex items-center justify-center bg-white rounded-t-[10px] overflow-hidden";
+
+  const imageClassName = "w-full h-full object-contain p-4";
 
   const titleClassName =
-    "flex items-center justify-center p-[10px] bg-black text-white h-[fit-content bg-black rounded-b-[10px]";
+    "flex items-center justify-center p-[10px] bg-black text-white rounded-b-[10px]";
 
   return (
     <div className={boxClassName} onClick={onClick}>
-      <ImageBox
-        wrapperClassName={imageWrapperClassName}
-        imageClassName={imageClassName}
-        Image={Thumbnail}
-        width={width}
-        height={height}
-      />
+      <div className={imageWrapperClassName}>
+        <Thumbnail className={imageClassName} />
+      </div>
       <div className={titleClassName}>{title}</div>
     </div>
   );
