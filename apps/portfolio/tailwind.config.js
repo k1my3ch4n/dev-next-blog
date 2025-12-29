c785c16865fa1a770673e82203bcdd4b342f9dcf
Kim <kevin2695@naver.com>
2025-12-29 16:37:26 +0900

    refactor : tailwind 버전 업데이트 따른 수정

--- apps/portfolio/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const config = {
-  darkMode: "class",
-  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
-  theme: {},
-  plugins: [],
-};
-
-export default config;
