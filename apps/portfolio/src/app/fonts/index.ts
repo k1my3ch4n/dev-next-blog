ed031aa47572ab3c0143801dd39cf284105e4064
Kim <kevin2695@naver.com>
2025-12-30 18:05:20 +0900

    feat : 폰트 수정

--- apps/portfolio/src/app/fonts/index.ts
+++ apps/portfolio/src/app/fonts/index.ts
@@ -5,51 +5,53 @@ const PaperLogyFont = localFont({
     {
       path: "./Paperlogy-1Thin.woff2",
       weight: "100",
-      style: "thin",
+      style: "normal",
     },
     {
       path: "./Paperlogy-2ExtraLight.woff2",
       weight: "200",
-      style: "extraLight",
+      style: "normal",
     },
     {
       path: "./Paperlogy-3Light.woff2",
       weight: "300",
-      style: "light",
+      style: "normal",
     },
     {
       path: "./Paperlogy-4Regular.woff2",
       weight: "400",
-      style: "regular",
+      style: "normal",
     },
     {
       path: "./Paperlogy-5Medium.woff2",
       weight: "500",
-      style: "medium",
+      style: "normal",
     },
     {
       path: "./Paperlogy-6SemiBold.woff2",
       weight: "600",
-      style: "semiBold",
+      style: "normal",
     },
     {
       path: "./Paperlogy-7Bold.woff2",
       weight: "700",
-      style: "bold",
+      style: "normal",
     },
     {
       path: "./Paperlogy-8ExtraBold.woff2",
       weight: "800",
-      style: "extraBold",
+      style: "normal",
     },
     {
       path: "./Paperlogy-9Black.woff2",
       weight: "900",
-      style: "black",
+      style: "normal",
     },
   ],
   display: "swap",
   variable: "--font-paperlogy",
+  preload: true,
+  fallback: ["system-ui", "sans-serif"],
 });
 
 export default PaperLogyFont;
