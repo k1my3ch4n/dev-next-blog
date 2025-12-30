7cc04e36bed5effaac602ac669a408ce5520122a
Kim <kevin2695@naver.com>
2025-12-30 15:38:40 +0900

    feat : aria 추가

--- packages/components/src/Divider.tsx
+++ packages/components/src/Divider.tsx
@@ -1,7 +1,7 @@
 const Divider = () => {
   const classNames = "w-full h-[1px] bg-[var(--theme-divider)] my-[10px] mx-0";
 
-  return <div className={classNames}></div>;
+  return <div className={classNames} role="separator" aria-hidden="true" />;
 };
 
 export default Divider;
--- packages/components/src/ScrollToTopButton.tsx
+++ packages/components/src/ScrollToTopButton.tsx
@@ -11,6 +11,7 @@ const ScrollToTopButton = () => {
         <button
           className="fixed bottom-[20px] right-[20px] px-[20px] py-[10px] cursor-pointer rounded-[40px] shadow-inner-border"
           onClick={scrollToTop}
+          aria-label="페이지 맨 위로 스크롤"
         >
           맨 위로
         </button>
