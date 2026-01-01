92d5b222905d5670ecf129a0209692360e60d49e
Kim <kevin2695@naver.com>
2026-01-01 18:22:35 +0900

    feat : blog 페이지 원상 복구

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -1,24 +1,18 @@
-import Maintenance from "./_components/Maintenance";
+import prefetchHomeData from "@prefetcher/prefetchHomeData";
+import Blog from "./_components/Blog";
+import Extra from "./_components/Extra";
+import Introduce from "./_components/Introduce";
+import Projects from "./_components/Projects";
 
-// import prefetchHomeData from "@prefetcher/prefetchHomeData";
-// import Blog from "./_components/Blog";
-// import Extra from "./_components/Extra";
-// import Introduce from "./_components/Introduce";
-// import Projects from "./_components/Projects";
-//
-// export default async function Home() {
-//   await prefetchHomeData();
-//
-//   return (
-//     <>
-//       <Introduce />
-//       <Projects />
-//       <Blog />
-//       <Extra />
-//     </>
-//   );
-// }
+export default async function Home() {
+  await prefetchHomeData();
 
-export default function Home() {
-  return <Maintenance />;
+  return (
+    <>
+      <Introduce />
+      <Projects />
+      <Blog />
+      <Extra />
+    </>
+  );
 }
