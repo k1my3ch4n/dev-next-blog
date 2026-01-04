843883a37049eb11970b59f38992c2a2bed8e8e8
Kim <kevin2695@naver.com>
2026-01-04 17:19:16 +0900

    feat : schemaLink 설정 추가

--- apps/blog/src/client/client.ts
+++ apps/blog/src/client/client.ts
@@ -1,4 +1,6 @@
 import { HttpLink } from "@apollo/client";
+import { SchemaLink } from "@apollo/client/link/schema";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 import {
   registerApolloClient,
@@ -6,26 +8,28 @@ import {
   ApolloClient,
 } from "@apollo/client-integration-nextjs";
 
+import { typeDefs, resolvers } from "@/lib/graphql/schema";
+
 const VALIDATED_API_KEY = process.env.VALIDATED_API_KEY || "";
 
-// 서버 사이드에서는 절대 URL 필요
-const getGraphQLUri = () => {
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const getLink = () => {
   if (typeof window === "undefined") {
-    // 서버 사이드: 환경변수 또는 기본값 사용
-    return process.env.GRAPHQL_URL || "http://localhost:3000/api/graphql";
+    return new SchemaLink({ schema });
   }
-  // 클라이언트 사이드: 상대 URL 사용
-  return "/api/graphql";
+
+  return new HttpLink({
+    uri: "/api/graphql",
+    headers: {
+      "x-api-key": VALIDATED_API_KEY,
+    },
+  });
 };
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      uri: getGraphQLUri(),
-      headers: {
-        "x-api-key": VALIDATED_API_KEY,
-      },
-    }),
+    link: getLink(),
   });
 });
