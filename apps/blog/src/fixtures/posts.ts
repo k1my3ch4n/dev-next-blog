7c2b3b4b286bc7a69288f5d13ce0eefbfbd23eef
Kim <kevin2695@naver.com>
2026-02-04 21:02:26 +0900

    refactor : 서버에서 게시글 데이터 뿐만 아니라 외부 url 을 받을 수 있도록 수정

--- apps/blog/src/fixtures/posts.ts
+++ apps/blog/src/fixtures/posts.ts
@@ -1,6 +1,8 @@
 export interface PostData {
   id: number;
-  postKey: string;
+  postKey: string | null;
+  externalUrl: string | null;
+  thumbnailKey: string | null;
   title: string;
   tags: string[];
 }
@@ -18,18 +20,24 @@ export const MOCK_GET_POSTS_RESPONSE: PostsResponseData = {
     {
       id: 1,
       postKey: 'msw-1',
+      externalUrl: null,
+      thumbnailKey: 'msw-1',
       title: 'title1',
       tags: ['tag1', 'tag2'],
     },
     {
       id: 2,
       postKey: 'msw-2',
+      externalUrl: null,
+      thumbnailKey: 'msw-2',
       title: 'title2',
       tags: ['tag3', 'tag4'],
     },
     {
       id: 3,
       postKey: 'msw-3',
+      externalUrl: null,
+      thumbnailKey: 'msw-3',
       title: 'title3',
       tags: ['tag5', 'tag6'],
     },
@@ -40,6 +48,8 @@ export const MOCK_GET_POST_WITH_KEY_RESPONSE: PostWithKeyResponseData = {
   post: {
     id: 1,
     postKey: 'msw-1',
+    externalUrl: null,
+    thumbnailKey: 'msw-1',
     title: 'title1',
     tags: ['tag1', 'tag2'],
   },
