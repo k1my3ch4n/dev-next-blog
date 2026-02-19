2789197ba3e042af3432405d4ac5ff88338284a2
Kim <kevin2695@naver.com>
2026-02-19 22:29:06 +0900

    refactor : SplitGrid 반응형 수정

--- packages/components/src/SplitGrid.tsx
+++ packages/components/src/SplitGrid.tsx
@@ -9,11 +9,11 @@ const SplitGrid = ({
   rhs: React.ReactNode;
   rhsClassName?: string;
 }) => {
-  const wrapperClassName = "grid grid-cols-[2fr_3fr] w-full";
+  const wrapperClassName = "grid grid-cols-1 md:grid-cols-[2fr_3fr] gap-4 w-full";
 
   return (
     <div className={wrapperClassName}>
-      <div className={lhsClassName}>{lhs}</div>
+      <div className={`flex flex-col items-center md:items-start ${lhsClassName}`}>{lhs}</div>
       <div className={rhsClassName}>{rhs}</div>
     </div>
   );
